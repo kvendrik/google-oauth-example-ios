ad0c662d90992786b5d0e680642c3ca87fce04dd
Koen Vendrik <k.vendrik@gmail.com>
2021-09-15 10:46:53 -0400

    Initial Commit

---
+++ google-oauth-example/google_oauth_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  google_oauth_exampleApp.swift
+//  google-oauth-example
+//
+//  Created by Koen Vendrik on 2021-09-15.
+//
+
+import SwiftUI
+
+@main
+struct google_oauth_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
